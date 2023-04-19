c408e5bee7d2d3aa72e9734c28b8f92446a37730
Matheus Oliveira <matheuseu41@gmail.com>
2023-04-19 19:11:11 -0300

    Commit inicial

---
+++ src/components/Formulario/index.jsx
@@ -0,0 +1,73 @@
+import { useEffect, useState } from "react"
+
+const Formulario = () => {
+    const [materiaA, setMateriaA] = useState(0);
+    const [materiaB, setMateriaB] = useState(0);
+    const [materiaC, setMateriaC] = useState(0);
+    const [nome, setNome] = useState('');
+
+    // mount
+    // update
+    // onmount
+
+    useEffect(() => {
+        console.log("O componente iniciou.")
+    }, []);
+
+    useEffect(() => {
+        console.log("O estado nome mudou");
+        return () => {
+            console.log("O componente finalizou")
+        }
+    }, [nome])
+
+    useEffect(() => {
+        console.log("Materia A mudou para :" + materiaA)
+    }, [materiaA, materiaB, materiaC])
+
+    const alteraNome = (evento) => {
+        //console.log(evento.target.value);
+        //setNome(evento.target.value);
+        setNome(estadoAnterior => {
+            console.log(estadoAnterior);
+
+            return evento.target.value;
+        })
+    }
+
+    const renderizaResultado = () => {
+        const soma = materiaA + materiaB + materiaC;
+        const media = soma / 3;
+
+        if (media >= 7) {
+            return (
+                <p>Boa {nome}, você foi aprovado!</p>
+            )
+        } else {
+            return (
+                <p>Vish {nome}, você não foi aprovado mané.</p>
+            )
+        }
+    }
+
+    return (
+        <form>
+            <ul>
+                {[1, 2, 3, 4, 5].map(item => (
+                    <li key={item}>{item}</li>)
+                    
+                )}
+                
+            </ul>
+
+            <input type="text" placeholder="Seu nome" onChange={alteraNome} />
+            <input type="number" placeholder="Nota matéria A" onChange={({ target}) => setMateriaA(parseInt(target.value))}/>
+            <input type="number" placeholder="Nota matéria B" onChange={evento => setMateriaB(parseInt(evento.target.value))} />
+            <input type="number" placeholder="Nota matéria C" onChange={evento => setMateriaC(parseInt(evento.target.value))} />
+            {renderizaResultado()}
+            
+        </form>
+    ) 
+}
+
+export default Formulario
